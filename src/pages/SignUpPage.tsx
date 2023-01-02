a2220eb2b0ab8fb9b768bf2a81a9bcf1d7bd58ab
Manyonge <77201345+Manyonge@users.noreply.github.com>
2023-01-02 04:46:35 +0300

    add changes to signup page

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,6 +1,13 @@
 import React, { FunctionComponent } from "react";
 import { ColumnBox, ErrorTypography, RowBox } from "../components";
-import { Button, Paper, TextField, Typography, useTheme } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  Paper,
+  TextField,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { SubmitHandler, useForm } from "react-hook-form";
 import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import { useMutation } from "react-query";
@@ -44,42 +51,78 @@ const SignUpPage: FunctionComponent<Props> = (props) => {
       }}
     >
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Paper elevation={4} sx={{ minWidth: "50vw", p: "3rem" }}>
-          <ColumnBox>
+        <Paper
+          elevation={4}
+          sx={{
+            width: "27vw",
+            py: "5%",
+            px: "10%",
+            height: "60vh",
+            borderRadius: "1rem",
+          }}
+        >
+          <ColumnBox
+            sx={{
+              width: "100%",
+              height: "100%",
+              justifyContent: "space-between",
+            }}
+          >
             {" "}
-            <Typography variant={"h6"} fontWeight={"bold"}>
-              Username
-            </Typography>
-            <TextField
-              placeholder={"Enter username"}
-              {...register("username", { required: "This field is required" })}
-            />
-            {errors.username && (
-              <ErrorTypography msg={errors.username.message} />
-            )}{" "}
-            <Typography variant={"h6"} fontWeight={"bold"}>
-              Email
-            </Typography>
-            <TextField
-              placeholder={"Enter email"}
-              {...register("email", { required: "This field is required" })}
-            />
-            {errors.email && <ErrorTypography msg={errors.email.message} />}
-            <Typography variant={"h6"} fontWeight={"bold"}>
-              Password
-            </Typography>
-            <TextField
-              placeholder={"Enter password"}
-              {...register("password", { required: "This field is required" })}
-            />
-            {errors.password && (
-              <ErrorTypography msg={errors.password.message} />
-            )}
-            <RowBox sx={{ pt: "3%", width: "30%" }}>
+            <RowBox sx={{ width: "100%" }}>
+              <Typography variant={"h6"} fontWeight={"bold"}>
+                Welcome
+              </Typography>
+            </RowBox>
+            <ColumnBox sx={{ width: "100%", alignItems: "flex-start" }}>
+              <Typography variant={"h6"}>Username</Typography>
+              <FormControl fullWidth>
+                <TextField
+                  size={"small"}
+                  variant={"filled"}
+                  placeholder={"Enter username"}
+                  {...register("username", {
+                    required: "This field is required",
+                  })}
+                />
+                {errors.username && (
+                  <ErrorTypography msg={errors.username.message} />
+                )}{" "}
+              </FormControl>
+            </ColumnBox>
+            <ColumnBox sx={{ width: "100%", alignItems: "flex-start" }}>
+              <Typography variant={"h6"}>Email</Typography>
+              <FormControl fullWidth>
+                <TextField
+                  size={"small"}
+                  variant={"filled"}
+                  placeholder={"Enter email"}
+                  {...register("email", { required: "This field is required" })}
+                />
+                {errors.email && <ErrorTypography msg={errors.email.message} />}
+              </FormControl>
+            </ColumnBox>
+            <ColumnBox sx={{ width: "100%", alignItems: "flex-start" }}>
+              <Typography variant={"h6"}>Password</Typography>
+              <FormControl fullWidth>
+                <TextField
+                  size={"small"}
+                  variant={"filled"}
+                  placeholder={"Enter password"}
+                  {...register("password", {
+                    required: "This field is required",
+                  })}
+                />
+                {errors.password && (
+                  <ErrorTypography msg={errors.password.message} />
+                )}
+              </FormControl>
+            </ColumnBox>
+            <RowBox sx={{ pt: "3%", width: "100%" }}>
               <Button
                 type={"submit"}
                 variant={"contained"}
-                sx={{ borderRadius: "3rem", width: "100%" }}
+                sx={{ borderRadius: "0.5rem", width: "100%" }}
               >
                 {"Sign up"}
               </Button>
